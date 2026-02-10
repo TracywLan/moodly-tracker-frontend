a995b9344d91573a9efbddea29b01b22fc5bc7c6
jillian <kjillian2025@gmail.com>
2026-02-10 10:49:01 -0600

    Added MoodDetails.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MoodDetails from './MoodDetails/MoodDetails'
 
 const App = () => {
   return <h1>Hello world!</h1>;
