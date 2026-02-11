8fa48f9c1e8cacd928718640f90f106a0d74f3ec
Luis Rojas <lmrojasvilla@gmail.com>
2026-02-11 15:55:07 -0500

    debugged add edit and delete for mood entries

--- src/components/MoodList/MoodList.jsx
+++ src/components/MoodList/MoodList.jsx
@@ -1,41 +1,47 @@
-import { Link } from 'react-router';
-import { UserContext } from '../../contexts/UserContext';
-import { useContext } from 'react';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 
-const MoodList = (props) => {
-    const { user } = useContext(UserContext)
+const MoodList = ({ moods }) => {
+  const { user } = useContext(UserContext);
 
-    const filteredMoods = props.moods.filter(mood => {
+    const filteredMoods = moods.filter((mood) => {
         if (!mood.author) return false;
-
         return mood.author?._id?.toString() === user?._id?.toString();
-    })
-    const sortMoods = filteredMoods.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    });
 
-    
+    const sortedMoods = filteredMoods.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
 
-    return (
-        <main>
-            <h1>Your Mood Entries</h1>
-            <Link to ="/moods/new" className="new-mood-btn">+ Add New Entry</Link>
-            {sortMoods.map((mood) => (
-                <div key={mood._id}>
-                    <Link to={`/moods/${mood._id}`}>
-                        <article>
-                            <header className="mood-list-header">
-                                <h2>{mood.rating}</h2>
-                                <p>{`${new Date(mood.createdAt).toLocaleDateString()}`}</p>
-                            </header>
-                        </article>
-                    </Link>
-                    <div>
-                        <p>{mood.moodLabel}</p>
-                        <p>{mood.note}</p>
-                    </div>
-                </div>
-            ))}
-        </main>
-    )
+  return (
+    <main>
+      <h1>Your Mood Entries</h1>
+      <Link to="/moods/new" className="new-mood-btn">
+        + Add New Entry
+      </Link>
+
+      {sortedMoods.map((mood) => {
+
+        return (
+          <div key={mood._id} style={{ border: "1px solid #ccc", padding: "10px", margin: "10px 0" }}>
+            <Link to={`/moods/${mood._id}`}>
+              <header className="mood-list-header">
+                <h2>{mood.rating}</h2>
+                <p>{new Date(mood.createdAt).toLocaleDateString()}</p>
+              </header>
+              <div>
+                <p>{mood.moodLabel}</p>
+                <p>{mood.note}</p>
+              </div>
+            </Link>
+
+
+          </div>
+        );
+      })}
+    </main>
+  );
 };
 
 export default MoodList;
