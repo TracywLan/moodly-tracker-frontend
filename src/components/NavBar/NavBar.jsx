eda805cd7c1ed6c903ea1fdd0a3c27753ca41a55
Tracywlan <tracylan12@gmail.com>
2026-02-12 20:36:04 -0500

    changed some css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -29,6 +29,9 @@ const NavBar = () => {
                 </ul>
             ) : (
                 <ul>
+                    <li><Link to='/' >HOME</Link></li>
+                    <li><Link to='/sign-in' >Sign In</Link></li>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
                 </ul>
             )}
             </div>
