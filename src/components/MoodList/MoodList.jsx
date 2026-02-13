8e211956acc4e9ed6403e497600cf90361a038e7
Tracy <174064146+TracywLan@users.noreply.github.com>
2026-02-13 13:11:22 -0500

    Change evening greeting to afternoon for midday

--- src/components/MoodList/MoodList.jsx
+++ src/components/MoodList/MoodList.jsx
@@ -20,7 +20,7 @@ const MoodList = ({ moods }) => {
     const getGreeting = () => {
         const hour = new Date().getHours();
         if (hour < 12) return 'Good Morning';
-        if (hour < 18 ) return 'Good Evening';
+        if (hour < 18 ) return 'Good Afternoon';
         return 'Good Evening';
     }
     
